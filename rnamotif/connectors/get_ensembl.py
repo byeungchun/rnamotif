524be80b167bed9b1aa6fd54bfdd3e34cf34eade
byeungchun.kwon <byeungchun.kwon@gmail.com>
2021-02-10 23:47:15 +0100

    add ensembl api return file save module (ensembl_rna_handler.py)

--- rnamotif/connectors/get_ensembl.py
+++ rnamotif/connectors/get_ensembl.py
@@ -1,17 +1,20 @@
+"""Ensembel REST API helper"""
+import os
 import json
 import time
 import logging
 import requests
 
 logger = logging.getLogger(__name__)
-c_handler = logging.StreamHandler()
-c_handler.setLevel(logging.DEBUG)
-c_format = logging.Formatter('%(levelname)s - %(message)s')
-logger.addHandler(c_handler)
+# c_handler = logging.StreamHandler()
+# c_handler.setLevel(logging.DEBUG)
+# c_format = logging.Formatter('%(levelname)s - %(message)s')
+# logger.addHandler(c_handler)
 
 MAX_RETRY = 5
 ENSEMBL_API_URL = "https://rest.ensembl.org/sequence/id/"
-HEADER = {"Content-Type" : "text/plain"}
+HEADER = {"Content-Type": "text/plain"}
+
 
 def get_ensembl_seq_by_gene(
     lst_gene: list,
@@ -31,36 +34,42 @@ def get_ensembl_seq_by_gene(
     """
 
     res = dict()
-    for gene in lst_gene:
+    for i, gene in enumerate(lst_gene):
         num_retry = 0
         flag_req_success = False
         res[gene] = dict()
         param = f'{gene}?type={seq_type}'
         req_url = f'{ENSEMBL_API_URL}{param}'
-        
+
         try:
             while num_retry < MAX_RETRY:
                 # Request API service
-                ret = requests.get(req_url, headers=HEADER,verify=is_ssl_verification)
+                ret = requests.get(req_url, headers=HEADER,
+                                   verify=is_ssl_verification)
                 if ret.status_code == 200:
                     flag_req_success = True
                 elif ret.status_code in [400, 404]:
                     res[gene][seq_type] = json.loads(ret.text)
                 elif ret.status_code in [403, 408, 429]:
-                    logger.info(f"Ensembl API server is not working for {req_url}.\nWait 5 seconds and retry. # of retries: {num_retry}/{MAX_RETRY}")
+                    logger.info(
+                        f"Ensembl API server is not working for {req_url}.\nWait 5 seconds and retry. # of retries: {num_retry}/{MAX_RETRY}")
                     time.sleep(5)
                     num_retry += 1
                     continue
                 elif ret.status_code == 503:
                     logger.error(f'Error({ret.status_code}): {ret.text}')
                 else:
-                    logger.error(f'Unknown server status code({ret.status_code})')
-                
-                
-                res[gene][seq_type] = {'retsts': flag_req_success, 'retval': ret.text}
+                    logger.error(
+                        f'Unknown server status code({ret.status_code})')
+
+                res[gene][seq_type] = {
+                    'retsts': flag_req_success,
+                    'requrl': req_url,
+                    'retval': ret.text
+                }
                 break
         except Exception as ex:
-            RuntimeError(f'Ensembl API request error.\n{ex}')
-        logger.info(f'FINISH - {req_url}')
+            raise RuntimeError(f'Ensembl API request error.\n{ex}')
+        print(f'{i} - {req_url}')
+
     return res
-                
